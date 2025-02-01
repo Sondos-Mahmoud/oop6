80f3bb809d2dc7f1d9729ce84e844f19724967ca
Sondos-Mahmoud <sondos.mohamed02@eng-st.cu.edu.eg>
2025-02-01 06:58:35 +0200

    oop6

---
+++ oop6/3D Point.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop6
+{
+    internal class _3D_Point: ICloneable, IComparable<_3D_Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public _3D_Point()  
+        {
+            X = 0;
+            Y = 0;  
+            Z = 0;
+        }
+
+
+        public _3D_Point(int x)
+        {
+        X=x; Y = 0;Z = 0;
+        }
+
+        public _3D_Point(int x, int y) 
+        {
+            X = x;
+            Y=y;
+            Z = 0;
+        }
+        public _3D_Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $" “Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is _3D_Point other)
+                return X == other.X && Y == other.Y && Z == other.Z;
+            return false;
+        }
+        public static bool operator ==(_3D_Point p1, _3D_Point p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(_3D_Point p1, _3D_Point p2)
+        {
+            return !p1.Equals(p2);
+        }
+        public object Clone()
+        {
+            return new _3D_Point(X,Y,Z); 
+
+        }
+
+        public int CompareTo(_3D_Point? other)
+        {
+            if (X != other.X)
+                return X.CompareTo(other.X);
+            return Y.CompareTo(other.Y); 
+        }
+        public static _3D_Point ReadPoint()
+        {
+            int x = ReadInt("Enter X: ");
+            int y = ReadInt("Enter Y: ");
+            int z = ReadInt("Enter Z: ");
+            return new _3D_Point(x, y, z);
+        }
+        private static int ReadInt(string message)
+        {
+            int value;
+            while (true)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out value)) return value;
+
+               
+            }
+        }
+    }
+
+
+}
--- oop6/Program.cs
+++ oop6/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-           
+            #region 3D point
+            _3D_Point P1 = _3D_Point.ReadPoint();
+            _3D_Point P2 = _3D_Point.ReadPoint();
+            Console.WriteLine($"P1: {P1}");
+            Console.WriteLine($"P2: {P2}");
+            Console.WriteLine($"P1 == P2: {P1 == P2}");
+            _3D_Point P1Clone = (_3D_Point)P1.Clone();
+            Console.WriteLine($"Cloned P1: {P1Clone}");
+
+            // Sorting an array of points
+            _3D_Point[] points = { new _3D_Point(3, 2, 1), new _3D_Point(1, 5, 2), new _3D_Point(2, 3, 4), new _3D_Point(1, 2, 3) };
+            Array.Sort(points);
+
+            Console.WriteLine("\nSorted Points:");
+            foreach (var point in points) Console.WriteLine(point);
+
+            #endregion
+            #region Calling Math Functions
+
+            int num1 = 10, num2 = 5;
+
+            Console.WriteLine($"Add: {Math.Add(num1, num2)}");
+            Console.WriteLine($"Subtract: {Math.Subtract(num1, num2)}");
+            Console.WriteLine($"Multiply: {Math.Multiply(num1, num2)}");
+            Console.WriteLine($"Divide: {Math.Divide(num1, num2)}");
+            
+            #endregion
+
         }
     }
 }
